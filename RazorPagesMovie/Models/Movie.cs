b5c29145c309d4752e6e381b3a5d3a60581136dc
sqw0shh <starikh3sus@gmail.com>
2023-06-26 10:27:57 +0300

    First commit

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {public int Id { get; set; }
+        public string? Title { get; set; } = string.Empty;
+        [Display(Name = "Дата выпуска")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
